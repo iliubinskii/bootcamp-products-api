b1a68e2402e9a1b7ce4e513ed544eee952d7ab31
Ilia Liubinskii <ilia.liubinskii@gmail.com>
2024-04-18 00:09:05 +0300

    Added template for cloudinary service

---
+++ src/providers/web-accessible-file-storage/cloudinary.js
@@ -0,0 +1,9 @@
+function createCloudinaryStore(config) {
+  return {
+    uploadFile: async (filename, pathToTempFile) => {
+      // TODO
+
+      return "<URL>"; // URL if the upload was successful, null otherwise
+    },
+  };
+}
