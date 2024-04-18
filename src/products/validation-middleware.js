00c6d00ae1cd3af713418f89d5350fb6a103f948
mvdem <m9602598255@gmail.com>
2024-04-18 23:31:31 +0300

    #29 Make validation middleware for products

--- src/products/validation-middleware.js
+++ src/products/validation-middleware.js
@@ -1,14 +1,36 @@
-export default {
-  validateCreateProduct: (req, res, next) => {
-    // TODO
-    next();
-  },
-  validateUpdateProduct: (req, res, next) => {
-    // TODO
-    next();
-  },
-  requireExistingCategories: (req, res, next) => {
-    // TODO
-    next();
-  },
+import createSchema from './validation-scheme/createSchema.js';
+import updateSchema from './validation-scheme/updateSchema.js';
+
+const options = {
+  abortEarly: false, // report all errors, not just the first
+  stripUnknown: true, // remove unknown keys
 };
+
+function mapErrorDetailsToObj(details) {
+  return details?.reduce((acc, err) => {
+    acc[err.path] = err.message;
+    return acc;
+  }, {});
+}
+
+const createProduct = async (req, res, next) => {
+  //logger.info(`Validating create book request body req:${req.uuid}`);
+  try {
+    const result = await createSchema.validateAsync(req.body, options);
+    return next();
+  } catch (error) {
+    return res.status(500).json({ error: mapErrorDetailsToObj(error.details) });
+  }
+};
+
+const updateProduct = async (req, res, next) => {
+  //logger.info(`Validating create book request body req:${req.uuid}`);
+  try {
+    const result = await updateSchema.validateAsync(req.body, options);
+    return next();
+  } catch (error) {
+    return res.status(500).json({ error: mapErrorDetailsToObj(error.details) });
+  }
+};
+
+export default { createProduct, updateProduct };
